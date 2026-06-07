7d0769fbe4b3c599058ed2e12582beb69c55a085
g0dfathera <paata2009metreveli@gmail.com>
2026-06-07 16:20:41 +0400

    New Homework

---
+++ Homework/Homework1.cs
@@ -0,0 +1,219 @@
+﻿// Task 1
+
+Console.WriteLine("Write your Name");
+var Name = Console.ReadLine();
+
+Console.WriteLine();
+
+Console.WriteLine("Write your first exam score");
+var firstExamScore = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine("Write your second exam score");
+var secondExamScore = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine("Write your homework score");
+var homeworkScore = int.Parse(Console.ReadLine());
+
+var averageScore = (firstExamScore + secondExamScore + homeworkScore) / 3;
+
+if (averageScore > 100 || averageScore < 0)
+{
+    Console.WriteLine("არასწორი ქულა");
+}
+else if (averageScore >= 90)
+{
+    Console.WriteLine("თქვენი შეფასებაა - A");
+    Console.WriteLine("ჩააბარა");
+}
+else if (averageScore >= 80)
+{
+    Console.WriteLine("თქვენი შეფასებაა - B");
+    Console.WriteLine("ჩააბარა");
+}
+else if (averageScore >= 70)
+{
+    Console.WriteLine("თქვენი შეფასებაა - C");
+    Console.WriteLine("ჩააბარა");
+}
+else if (averageScore >= 60)
+{
+    Console.WriteLine("თქვენი შეფასებაა - D");
+    Console.WriteLine("ჩააბარა");
+}
+else
+{
+    Console.WriteLine("თქვენი შეფასებაა - F");
+    Console.WriteLine("ვერ ჩააბარა");
+}
+
+Console.WriteLine();
+
+// Task 2
+
+int Balance = 1000;
+int amount;
+int input;
+
+do
+{
+    Console.WriteLine("მენიუ:");
+    Console.WriteLine("1 - ბალანსი");
+    Console.WriteLine("2 - თანხის შეტანა");
+    Console.WriteLine("3 - თანხის გამოტანა");
+    Console.WriteLine("4 - გამოსვლა");
+    Console.WriteLine();
+
+    input = int.Parse(Console.ReadLine());
+
+    switch (input)
+    {
+        case 1:
+            Console.WriteLine($"თქვენი ბალანსია: {Balance} ლარი");
+            break;
+
+        case 2:
+            Console.WriteLine("დაწერეთ შესატანი თანხის რაოდენობა:");
+            amount = int.Parse(Console.ReadLine());
+            Balance = Balance + amount;
+            break;
+        case 3:
+            Console.WriteLine("დაწერეთ გამოსატანი თანხის რაოდენობა:");
+            amount = int.Parse(Console.ReadLine());
+            if (Balance > amount)
+            {
+                Balance = Balance - amount;
+            }
+            else
+            {
+                Console.WriteLine("ანგარიშზე არ არის საკმარისი თანხა:");
+            }
+            break;
+    }
+}
+while (input != 4);
+
+Console.WriteLine();
+
+// Task 3
+
+
+Console.WriteLine("შეიყვანეთ რიცხვი:");
+var secondInput = int.Parse(Console.ReadLine());
+
+if (secondInput > 0)
+{
+    Console.WriteLine("რიცხვი დადებითია");
+}
+else if (secondInput == 0)
+{
+    Console.WriteLine("რიცხვი ნულია");
+}
+else
+{
+    Console.WriteLine("რიცხვი უარყოფითია");
+}
+
+if (secondInput % 2  == 0)
+{
+    Console.WriteLine("რიცხვი ლუწია");
+}
+else
+{
+    Console.WriteLine("რიცხვი კენტია");
+}
+
+for (int i = 1; i < 11; i++)
+{
+    Console.WriteLine(secondInput * i);
+}
+
+// Task 4
+
+string Username = "admin";
+string Password = "12345";
+string inputUsername;
+string inputPassword;
+
+var loopNumber = 0;
+
+while (loopNumber < 3)
+{
+    Console.WriteLine("შეიყვანეთ Username");
+    inputUsername = Console.ReadLine();
+
+    Console.WriteLine();
+
+    Console.WriteLine("შეიყვანეთ Password");
+    inputPassword = Console.ReadLine();
+
+    Console.WriteLine();
+    
+    if (inputUsername == Username && inputPassword == Password)
+    {
+        Console.WriteLine("კეთილი იყოს თქვენი მობრძანება");
+        loopNumber += 4;
+    }
+    else
+    {
+        Console.WriteLine("არასწორი მონაცემები");
+        loopNumber++;
+    }
+
+    if (loopNumber == 3) 
+    {
+        Console.WriteLine("ანგარიში დაიბლოკა");
+    }
+}
+
+// Task 5
+
+int menuChoice;
+
+Console.WriteLine("შეიყვანეთ პირველი რიცხვი");
+int thirdNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine("შეიყვანეთ მეორე რიცხვი");
+int fourthNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("მენიუ:");
+Console.WriteLine("1 - მიმატება");
+Console.WriteLine("2 - გამოკლება");
+Console.WriteLine("3 - გამრავლება");
+Console.WriteLine("4 - გაყოფა");
+Console.WriteLine();
+menuChoice = int.Parse(Console.ReadLine());
+switch (menuChoice)
+{
+    case 1:
+        Console.WriteLine(thirdNumber + fourthNumber);
+        Console.WriteLine();
+        break;
+    case 2:
+        Console.WriteLine(thirdNumber - fourthNumber);
+        Console.WriteLine();
+        break;
+    case 3:
+        Console.WriteLine(thirdNumber * fourthNumber);
+        Console.WriteLine();
+        break;
+    case 4:
+        if (fourthNumber == 0)
+        {
+            Console.WriteLine("შეცდომა");
+        }
+        else
+        {
+            Console.WriteLine(thirdNumber / fourthNumber);
+        }
+        break;
+    default:
+        Console.WriteLine("არასწორი ოპერაცია");
+        Console.WriteLine();
+        break;
+}
---
+++ Homework/Homework2.cs
@@ -0,0 +1,140 @@
+﻿// Task 1
+
+Console.WriteLine("რამდენი ციფრის შეყვანა გსურთ?");
+
+int amount = int.Parse(Console.ReadLine());
+
+var numbers = new int[amount];
+
+for  (int i = 0; i < amount;  i++)
+{
+    Console.WriteLine($"შეიყვანეთ რიცხვი ნომერი {i + 1}");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine();
+
+
+int sum = 0;
+int average = 0;
+int max = numbers[0];
+int min = numbers[0];
+
+Console.WriteLine("თქვენი შეყვანილი რიცხვებია: ");
+
+for (int i = 0; i < amount; i++)
+{
+    Console.Write(numbers[i] + "; ");
+
+    sum += numbers[i];
+
+    average += sum / amount;
+
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+
+Console.WriteLine();
+
+Console.WriteLine("თქვენი შეყვანილი რიცხვების ჯამია: " + sum);
+
+Console.WriteLine();
+
+Console.WriteLine("თქვენი შეყვანილი რიცხვების საშუალო მნიშვნელობაა: " + average);
+
+Console.WriteLine();
+
+Console.WriteLine("ყველაზე დიდი რიცხვი - " + max);
+
+Console.WriteLine();
+
+Console.WriteLine("ყველაზე პატარა რიცხვი - " + min);
+
+
+// Task 2
+
+Console.WriteLine("რამდენი სტუდენტის შეყვანა გსურთ?");
+
+int amount2 = int.Parse(Console.ReadLine());
+
+var students = new List<string>(amount2);
+
+for (int i = 0; i < amount2; i++)
+{
+    Console.WriteLine($"შეიყვანეთ სტუდენტი N{i + 1}");
+    students.Add(Console.ReadLine());
+}
+
+Console.WriteLine();
+
+int index = 1;
+
+foreach (string student in students)
+{
+    Console.WriteLine($"სტუდენტი {index}: {student}");
+    index ++;
+}
+
+Console.WriteLine($"ჯამში სტუდენტების რაოდენობა: {amount2}");
+
+
+// Task 3
+
+Console.Write("შეიყვანეთ სიტყვა ან წინადადება: ");
+string userInput = Console.ReadLine();
+
+AnalyzeText(userInput);
+
+void AnalyzeText(string text)
+{
+    int length = text.Length;
+
+    string upperText = text.ToUpper();
+
+    string lowerText = text.ToLower();
+
+    bool containsCSharp = text.Contains("CSharp");
+
+    string contains;
+
+    if (containsCSharp)
+    {
+        contains = "შეიცავს";
+    }
+    else { contains = "არ შეიცავს"; }
+
+    Console.WriteLine($"სიმბოლოების რაოდენობა: {length}");
+    Console.WriteLine($"დიდი ასოებით: {upperText}");
+    Console.WriteLine($"პატარა ასოებით: {lowerText}");
+    Console.WriteLine($"შეიცავს თუ არა სიტყვას 'CSharp': {contains}");
+}
+
+
+// Task 4
+
+string input = Console.ReadLine();
+string reversedResult = ReverseText(input);
+
+string ReverseText(string text)
+{
+    if (text.Length <= 1)
+    {
+        return text;
+    }
+
+    return text[text.Length - 1] + ReverseText(text.Substring(0, text.Length - 1));
+}
+
+Console.WriteLine();
+
+Console.Write("შეიყვანეთ სიტყვა: ");
+
+Console.WriteLine("შებრუნებული ტექსტი: " + reversedResult);
+
